97b8250dc21cf140a2f77a36130d0b5471f7505b
David Paucar <soydavidpaucar@gmail.com>
2022-11-14 17:22:11 -0500

    :art: created searchbar initial structure component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,10 @@
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" name="github-username" value="" />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default SearchBar;
