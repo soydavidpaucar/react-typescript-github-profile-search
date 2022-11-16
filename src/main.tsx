a8a7b15f59759c3c4cea28b92f85cd84994d779f
David Paucar <soydavidpaucar@gmail.com>
2022-11-15 21:24:08 -0500

    :art: imported theme and added to ChackraProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import GithubSearchProfileApp from './App';
 import './index.css';
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <GithubSearchProfileApp />
-  </React.StrictMode>
+  </ChakraProvider>
 );
