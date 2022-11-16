0431a6bd37afba734469685bba9fd068bd4ad2a5
David Paucar <soydavidpaucar@gmail.com>
2022-11-15 21:11:30 -0500

    :art: added form types and created call api on submit

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,47 @@
-function SearchBar() {
+/* eslint-disable react/jsx-props-no-spreading */
+import { Button, FormControl, FormErrorMessage, Input } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+  username: string;
+};
+
+type SearchBarProps = {
+  onSearchGithubUsername: (username: string) => Promise<void>;
+};
+
+function SearchBar({ onSearchGithubUsername }: SearchBarProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>();
+
+  const onSubmit = handleSubmit(async (data): Promise<void> => {
+    if (data.username.trim().length > 0) {
+      await onSearchGithubUsername(data.username);
+    }
+  });
+
   return (
-    <form>
-      <input type="text" name="github-username" value="" />
-      <button type="submit">Search</button>
+    <form onSubmit={onSubmit}>
+      <FormControl isInvalid={!!errors.username}>
+        <Input
+          {...register('username', {
+            required: 'GitHub username is required',
+          })}
+          defaultValue=""
+          type="text"
+          placeholder="Enter github username..."
+          width="auto"
+        />
+        <FormErrorMessage>
+          {errors.username && errors.username.message}
+        </FormErrorMessage>
+      </FormControl>
+      <Button type="submit" colorScheme="gray" isLoading={isSubmitting}>
+        Search
+      </Button>
     </form>
   );
 }
