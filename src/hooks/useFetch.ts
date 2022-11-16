26998b6ef6d836207d8cca939ff5ed5e0b61def2
David Paucar <soydavidpaucar@gmail.com>
2022-11-15 21:12:15 -0500

    :sparkles: improved information returned to component from github api

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,15 +1,64 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { useState } from 'react';
 
+type GithubApiResponse = {
+  login: string;
+  avatar_url: string;
+  html_url: string;
+  name: string;
+  location?: string;
+  bio?: string;
+  public_repos?: number;
+  followers?: number;
+  following?: number;
+  message?: string;
+};
+
 function useFetch() {
-  const [githubData, setGithubData] = useState<[]>([]);
+  const [githubData, setGithubData] = useState<GithubApiResponse>({
+    login: '',
+    avatar_url: '',
+    html_url: '',
+    name: '',
+    location: '',
+    bio: '',
+    public_repos: 0,
+    followers: 0,
+    following: 0,
+    message: '',
+  });
 
   const fetchGithubData = async (githubUsername: string): Promise<void> => {
     if (githubUsername !== githubData?.login) {
       const response: Response = await fetch(
         `https://api.github.com/users/${githubUsername}`
       );
-      const data: [] = await response.json();
-      setGithubData(data);
+      const data: GithubApiResponse = await response.json();
+      const {
+        login,
+        avatar_url,
+        html_url,
+        name,
+        location,
+        bio,
+        public_repos,
+        followers,
+        following,
+        message,
+      } = data;
+
+      setGithubData({
+        login,
+        avatar_url,
+        html_url,
+        name,
+        location,
+        bio,
+        public_repos,
+        followers,
+        following,
+        message,
+      });
     }
   };
 
