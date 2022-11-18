876ca1b39f58ac0392f0f5d6bdd7c4d6b0fbbf28
David Paucar <soydavidpaucar@gmail.com>
2022-11-17 20:22:03 -0500

    :art: added Home page and styled container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Home from './pages/Home';
+
 function GithubSearchProfileApp() {
-  return <h1>Hello World</h1>;
+  return (
+    <main className="max-h-screen bg-[#0a192f] h-screen">
+      <Home />
+    </main>
+  );
 }
 
 export default GithubSearchProfileApp;
