1f1774811c64206ff4e11b19c44c531d88682f7a
David Paucar <soydavidpaucar@gmail.com>
2022-11-18 16:28:26 -0500

    :art: imported components and change styles with tailwindcss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,23 @@
-import { Alert, AlertIcon } from '@chakra-ui/react';
 import SearchBar from '../components/SearchBar';
 import useFetch from '../hooks/useFetch';
+import Card from '../components/Card';
 
 function Home() {
   const { githubData, fetchGithubData } = useFetch();
 
   return (
-    <>
+    <section className="max-w-screen-md flex flex-col m-auto pt-52 px-12 sm:px-0">
+      <h1 className="text-center text-[#ccd6f6] mb-12 text-4xl md:text-6xl">
+        Github Search Profile
+      </h1>
       <SearchBar onSearchGithubUsername={fetchGithubData} />
       {githubData.message && (
-        <Alert status="error">
-          <AlertIcon />
+        <div className="border border-red-500 text-red-500 border text-center rounded-md mt-24 bg-red-500/20 shadow-lg shadow-red-500/50 backdrop-blur-sm py-4">
           Github user not found, please try again
-        </Alert>
+        </div>
       )}
-    </>
+      {Object.keys(githubData).length !== 0 && Card({ githubData })}
+    </section>
   );
 }
 
