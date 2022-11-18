f211bd24836d22ab435bc26828c8eefae94da647
David Paucar <soydavidpaucar@gmail.com>
2022-11-18 16:51:57 -0500

    :lipstick: fixed height on mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Home from './pages/Home';
 
 function GithubSearchProfileApp() {
   return (
-    <main className="max-h-screen bg-[#0a192f] h-screen">
+    <main className="min-h-screen bg-[#0a192f] max-h-full">
       <Home />
     </main>
   );
