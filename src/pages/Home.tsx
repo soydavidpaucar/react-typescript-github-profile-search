512441cabb296a582aca96089a7bec122d2def7d
David Paucar <soydavidpaucar@gmail.com>
2022-11-18 20:49:12 -0500

    :zap: updated data to send to child components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,27 @@
+import { useState } from 'react';
 import SearchBar from '../components/SearchBar';
-import useFetch from '../hooks/useFetch';
 import Card from '../components/Card';
 
 function Home() {
-  const { githubData, fetchGithubData } = useFetch();
+  const [username, setUsername] = useState<string>('');
+
+  const handleChange = (githubUsername: string): void => {
+    if (
+      username !== githubUsername.trim() &&
+      githubUsername.trim().length > 0
+    ) {
+      setUsername(githubUsername);
+    }
+  };
 
   return (
     <section className="max-w-screen-md flex flex-col m-auto pt-52 px-12 sm:px-0">
       <h1 className="text-center text-[#ccd6f6] mb-12 text-4xl md:text-6xl">
         Github Search Profile
       </h1>
-      <SearchBar onSearchGithubUsername={fetchGithubData} />
-      {githubData.message && (
-        <div className="border border-red-500 text-red-500 border text-center rounded-md mt-24 bg-red-500/20 shadow-lg shadow-red-500/50 backdrop-blur-sm py-4">
-          Github user not found, please try again
-        </div>
-      )}
-      {Object.keys(githubData).length !== 0 && Card({ githubData })}
+      <SearchBar onHandleChange={handleChange} />
+
+      {username && <Card githubUsername={username} />}
     </section>
   );
 }
